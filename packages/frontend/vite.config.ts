003b6d8b3ed9a13f9f0df6bd621317e7068c69bf
takejohn <takejohn@takejohn.jp>
2025-11-15 17:21:02 +0900

    Tailwind CSS導入

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dotenv from 'dotenv';
+import tailwindcss from '@tailwindcss/vite';
 
 dotenv.config({ path: '../../.env' });
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), tailwindcss()],
 	server: {
 		allowedHosts: process.env.APP_ORIGIN ? [new URL(process.env.APP_ORIGIN).host] : [],
 		proxy: {
